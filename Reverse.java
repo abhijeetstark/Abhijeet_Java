83bf9b8a43b50b4eee117f1652d8abcdf74aef66
abhijeetstark <abhijeet.parida07@gmail.com>
2017-08-16 18:22:30 +0530

    Changes added

--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,7 @@ public static void main(String[] args) {
   StringBuilder input1 = new StringBuilder();
   input1.append(input);
   input1=input1.reverse(); 
+  System.out.print("The reverse of the string is");
   for (int i=0;i<input1.length();i++)
   System.out.print(input1.charAt(i));  
  }}
